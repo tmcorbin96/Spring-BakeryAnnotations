9f8ccd28e7c5b354e895546584e8f86437e5acb2
tmcorbin96 <115915843+tmcorbin96@users.noreply.github.com>
2022-12-17 22:19:16 -0500

    all test passed

--- src/main/java/com/zipcodewilmington/bakery/repositories/BakerRepository.java
+++ src/main/java/com/zipcodewilmington/bakery/repositories/BakerRepository.java
@@ -2,6 +2,9 @@ package com.zipcodewilmington.bakery.repositories;
 
 import com.zipcodewilmington.bakery.models.Baker;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Repository
 public interface BakerRepository extends CrudRepository<Baker, Long> {
 }
